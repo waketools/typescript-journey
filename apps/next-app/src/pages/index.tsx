dddede7f0bbdac3145f16de6a97812e960bc670b
Simon Verhoeven <simon.verhoeven>
2023-10-05 11:40:33 +0700

    Use s3 bucket in next app

--- apps/next-app/src/pages/index.tsx
+++ apps/next-app/src/pages/index.tsx
@@ -63,11 +63,17 @@ function AuthShowcase() {
     { enabled: sessionData?.user !== undefined },
   );
 
+  const { data: url } = api.example.getUploadUrl.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined },
+  );
+
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-center text-2xl text-white">
         {sessionData && <span>Logged in as {sessionData.user?.email}</span>}
         {secretMessage && <span> - {secretMessage}</span>}
+        {url && <span> - {url}</span>}
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
--- apps/next-app/src/server/api/routers/example.ts
+++ apps/next-app/src/server/api/routers/example.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
 import { z } from "zod";
 
 import {
@@ -5,6 +7,9 @@ import {
   protectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { commonUtils } from "@ts-journey/common";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
@@ -17,5 +22,17 @@ export const exampleRouter = createTRPCRouter({
 
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
+  }),
+  getUploadUrl: protectedProcedure.query(async () => {
+    const command = new PutObjectCommand({
+      ACL: "public-read",
+      Key: crypto.randomUUID(),
+      Bucket: commonUtils.getS3BucketName(
+        commonUtils.BucketName.FILE_UPLOADS,
+        process.env.SST_STAGE!,
+      ),
+    });
+    const url = (await getSignedUrl(new S3Client({}), command)) as string;
+    return url;
   }),
 });
